8fbefd77596427df60e39b4f7c570c844af7096a
LuSc15 <luca.schmieding@gmail.com>
2024-11-05 15:55:23 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace LINQ___Sortieren_und_Partitionieren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0, 22, 12, 16, 18, 11, 19, 13 };
+            /*1.
+Geben Sie das obige Array aufsteigend sortiert aus
+2.
+Geben Sie das obige Array absteigend sortiert aus
+3.
+Geben Sie aus dem obigen Array alle graden Zahlen aufsteigend sortiert aus*/
+
+            var aufsteigend = numbers.OrderBy(x => x);
+            var absteigend = numbers.OrderByDescending(x => x);
+            var geradeAufsteigend = numbers.OrderBy(x => x).Where(x => x % 2 == 0);
+            Console.WriteLine("aufsteigend:");
+            foreach(var v in aufsteigend) Console.WriteLine(v);
+
+            Console.WriteLine("Absteigend:");
+            foreach(var v in absteigend)  Console.WriteLine(v);
+
+            Console.WriteLine("gerade aufsteigend:");
+            foreach(var v in geradeAufsteigend) Console.WriteLine(v);
+
+
+            string[] numbersString = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen" };
+
+            /*1.
+Geben Sie das obige Array nach der Länge der Worte aufsteigend sortiert aus
+2.
+Geben Sie das obige Array nach der Länge der Worte aufsteigend sortiert aus, bei gleicher Länge soll alphabetisch absteigend sortiert werden
+3.
+Drehen Sie die Reihenfolge der Elemente im Array um
+
+Erstellen Sie ein DirectoryInfo-Objekt für ein Verzeichnis Ihrer Wahl.
+4.
+Listen Sie alle Dateien in dem Verzeichnis, absteigend nach Namen sortiert auf
+5.
+Listen Sie alle Dateien in dem Verzeichnis, nach Größe aufsteigend sortiert auf
+6.
+Listen Sie alle Dateien in dem Verzeichnis, nach dem Datum des letzten Zugriffs auf, jüngste Dateien zuerst*/
+
+            var laengeAufsteigend = numbersString.OrderBy(x => x.Length);
+            var laengeAufsteigendDannAlphabetisch = numbersString.OrderBy(x => x.Length).ThenBy(x => x);
+            var drehen = numbersString.Reverse();
+            Console.WriteLine("Nach Länge aufsteigend:");
+            foreach(var v in laengeAufsteigend) Console.WriteLine(v);
+            Console.WriteLine("Länge aufsteigend dann alphabetisch:");
+            foreach(var v in laengeAufsteigendDannAlphabetisch) Console.WriteLine(v);
+            Console.WriteLine("umgekehrte Reihenfolge:");
+            foreach(var v in drehen) Console.WriteLine(v);
+
+            Console.WriteLine("Dateien aus C:Windows nach Namen sortiert:");
+            DirectoryInfo d = new DirectoryInfo(@"C:\Windows\");
+            var dateienNachName = d.GetFiles().OrderBy(x => x.Name);
+            foreach (var v in dateienNachName) Console.WriteLine(v.Name);
+
+            Console.WriteLine("Datei nach größe");
+            var dateienNachGroesse = d.GetFiles().OrderBy(x => x.Length);
+            foreach(var v in dateienNachGroesse) Console.WriteLine(v.Name+" "+v.Length);
+            Console.WriteLine("Dateien nach letztem Zugriff:");
+            var letzterZugriff = d.GetFiles().OrderBy(x => x.LastAccessTime.Ticks);
+            foreach(var v in letzterZugriff) Console.WriteLine(v.Name+" - "+v.LastAccessTime.ToString());
+
+
+            int[] numbers3 = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0, 22, 12, 16, 18, 11, 19, 13 };
+            var ersten5 = numbers3.Take(5);
+            foreach(var v in ersten5) Console.WriteLine(v);
+            var letzten5 = numbers.TakeLast(5);
+            foreach(var v in letzten5) Console.WriteLine(v);
+            var ohneErsteUndLetzten3 = numbers.Skip(3).SkipLast(3);
+            foreach(var v in ohneErsteUndLetzten3) Console.WriteLine(v);
+            var groesser0 = numbers.Where(x => x > 0);
+            foreach(var v in groesser0) Console.WriteLine(v);
+            var nach12 numbers.Skip(numbers.)
+
+
+
+        }
+        //public static void Ausgabe(List<T> sammlung,string name)
+        //{
+        //    Console.WriteLine(name);
+        //    foreach(T t in sammlung)
+        //    {
+        //        Console.WriteLine(t);
+        //    }
+        //}
+    }
+}
